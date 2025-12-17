ea5aa515ecc035f99b0db5275c26cc796fbdd613
Syed Muhammad Haris Shah <engrharisshah1122@gmail.com>
2025-12-17 12:13:27 +0500

    Add custom Thermal Insulation banner image - replaced generic banner with specific thermal insulation image for thermal-insulation service page

--- main-file/components/pages/common/breadcrumb.jsx
+++ main-file/components/pages/common/breadcrumb.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
-import image from "../../../public/assets/img/banner/page-banner-img.png";
+import defaultImage from "../../../public/assets/img/banner/page-banner-img.png";
 import shape from "../../../public/assets/img/shape/page-banner-shape.png";
 
-const BreadCrumb = ({title, innerTitle}) => {
+const BreadCrumb = ({title, innerTitle, customImage}) => {
+    const bannerImage = customImage || defaultImage;
+    
     return (
         <div className="page__banner">
             <div className="page__banner-shape">
@@ -17,7 +19,7 @@ const BreadCrumb = ({title, innerTitle}) => {
                     </div>
                     <div className="col-xl-4 col-lg-5">
                         <div className="page__banner-img">
-                            <img src={image.src} alt="image" />
+                            <img src={bannerImage.src} alt={title} />
                         </div>
                     </div>
                 </div>
--- main-file/components/pages/services/service-single/index.jsx
+++ main-file/components/pages/services/service-single/index.jsx
@@ -5,13 +5,21 @@ import BreadCrumb from "../../common/breadcrumb";
 import ServicesSingleMain from "./services-single";
 import FooterTwo from "@/components/layout/footers/footer-two";
 import ScrollToTop from "../../common/scroll/scroll-to-top";
+import thermalInsulationImg from "../../../../public/assets/img/Thermal Insulation.jpg";
 
 const ServicesSingle = ({serviceDetails}) => {
+    // Use custom banner image for thermal insulation service
+    const customImage = serviceDetails?.id === 'thermal-insulation' ? thermalInsulationImg : null;
+    
     return (
         <>
             <SEO pageTitle={`${serviceDetails?.title} - Hydro Seal Insulation Works`} />            
             <HeaderOne />
-            <BreadCrumb title={serviceDetails?.title} innerTitle={serviceDetails?.subtitle} />
+            <BreadCrumb 
+                title={serviceDetails?.title} 
+                innerTitle={serviceDetails?.subtitle} 
+                customImage={customImage}
+            />
             <ServicesSingleMain serviceDetails={serviceDetails}/>
             <FooterTwo />
             <ScrollToTop />
