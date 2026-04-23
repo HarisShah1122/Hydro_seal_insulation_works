72125be0d471e65141cecc608c08b43e1cf5aa29
Syed Muhammad Haris Shah <engrharisshah1122@gmail.com>
2026-04-23 15:00:05 +0500

    Update social media links: Add Facebook share URL and TikTok profile, remove Behance

--- main-file/components/data/social.jsx
+++ main-file/components/data/social.jsx
@@ -5,10 +5,9 @@ const Social = () => {
     return (
         <>
             <ul>
-                <li><Link href="https://facebook.com" target="_blank"><i className="fab fa-facebook-f"></i></Link></li>
-                <li><Link href="https://twitter.com" target="_blank"><i className="fa-brands fa-x-twitter"></i></Link></li>
-                <li><Link href="https://behance.net" target="_blank"><i className="fab fa-behance"></i></Link></li>
-                <li><Link href="https://linkedin.com" target="_blank"><i className="fab fa-linkedin-in"></i></Link></li>
+                <li><Link href="https://www.facebook.com/share/1a3G8Yry23/" target="_blank"><i className="fab fa-facebook-f"></i></Link></li>
+                <li><Link href="https://www.tiktok.com/@www.hsiwwaterproofing?_r=1&_t=ZS-950uhKkW93N" target="_blank"><i className="fab fa-tiktok"></i></Link></li>
+                                <li><Link href="https://linkedin.com" target="_blank"><i className="fab fa-linkedin-in"></i></Link></li>
             </ul>            
         </>
     );
